2b9f983e36ead26edd21d7413707b3df475154b7
lvbin <472245767@qq.com>
2018-12-21 17:35:35 +0800

    登录注册功能完善

--- server-loginSign/server.js
+++ server-loginSign/server.js
@@ -27,12 +27,39 @@ var server = http.createServer(function (request, response) {
         response.setHeader('Content-Type', 'text/html;charset=utf-8');
         response.write(string);
         response.end();
-    } else if (path === '/sign_up') {
+    } else if (path === '/sign_up' && method === 'GET') {
         let string = fs.readFileSync('./sign_up.html', 'utf-8');
         response.statusCode = 200;
         response.setHeader('Content-Type', 'text/html;charset=utf-8');
         response.write(string);
         response.end();
+    } else if (path === '/sign_up' && method === 'POST') {
+        readBody(request).then(function (body) {
+            let hash = [];
+            let strings = body.split('&'); // ['email=1', 'password=2', 'password_confirmation=3']
+            strings.forEach(function (string) {
+                let parts = string.split('=');
+                let key = parts[0];
+                let value = parts[1];
+                hash[key] = value;
+            });
+            let {email, password, password_confirmation} = hash;
+            if (email.indexOf('@') === -1) {
+                response.statusCode = 400;
+                response.setHeader('Content-Type', 'text/json;charset=utf-8');
+                response.write(`{
+                    "errors":{
+                        "email":"invalid"
+                    }
+                }`);
+            } else if (password !== password_confirmation) {
+                response.statusCode = 400;
+                response.write('password not match');
+            } else {
+                response.statusCode = 200;
+            }
+            response.end();
+        });
     } else if (path === '/main.js') {
         let string = fs.readFileSync('./main.js', 'utf-8');
         response.statusCode = 200;
@@ -63,7 +90,20 @@ var server = http.createServer(function (request, response) {
         `);
         response.end();
     }
-})
+});
+
+// 封装获取body数据的函数
+function readBody(request) {
+    return new Promise(function (resolve, reject) {
+        let body = [];
+        request.on('data', function (chunk) {
+            body.push(chunk);
+        }).on('end', function () {
+            body = Buffer.concat(body).toString();
+            resolve(body);
+        });
+    });
+}
 
 server.listen(port);
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port);
