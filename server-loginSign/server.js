8f00cdae4db4ba23ea6b0ab200ee8664a5c52cda
lvbin <472245767@qq.com>
2019-01-03 14:50:56 +0800

    使用Session保存Cookie信息

--- server-loginSign/server.js
+++ server-loginSign/server.js
@@ -1,69 +1,78 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var port = process.argv[2];
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const port = process.argv[2];
 
 if (!port) {
-    console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？');
+    console.log('请指定端口号:\nnode server.js 8888 类似于上面的格式');
     process.exit(1);
 }
 
-var server = http.createServer(function (request, response) {
-    var parsedUrl = url.parse(request.url, true);
-    var pathWithQuery = request.url;
-    var queryString = '';
+let sessions = {};
+
+let server = http.createServer(function (request, response) {
+    let parsedUrl = url.parse(request.url, true);
+    let pathWithQuery = request.url;
+    let queryString = '';
     if (pathWithQuery.indexOf('?') >= 0) {
         queryString = pathWithQuery.substring(pathWithQuery.indexOf('?'));
     }
-    var path = parsedUrl.pathname;
-    var query = parsedUrl.query;
-    var method = request.method;
+    let path = parsedUrl.pathname;
+    let query = parsedUrl.query;
+    let method = request.method;
 
     console.log('您好，含查询字符串的路径是：\n' + pathWithQuery);
 
     if (path === '/') {
-        var string = fs.readFileSync('./index.html', 'utf8');
-        var cookies = request.headers.cookie.split('; ');
-        var hash = {};
-        for (var i=0;i<cookies.length;i++) {
-           var parts = cookies[i].split('=');
-           var key = parts[0];
-           var value = parts[1];
-           hash[key] = value; 
+        let string = fs.readFileSync('./index.html', 'utf8');
+        let cookies = '';
+        if (request.headers.cookie) {
+            cookies = request.headers.cookie.split('; ');
         }
-        var email = hash.sign_in_email;
-        var users = fs.readFileSync('./db/users','utf8');
+        let hash = {};
+        for (let i = 0; i < cookies.length; i++) {
+            let parts = cookies[i].split('=');
+            let key = parts[0];
+            let value = parts[1];
+            hash[key] = value;
+        }
+        let mySessions = sessions[hash.sessionId];
+        let email;
+        if (mySessions) {
+            email = mySessions.sign_in_email;
+        }
+        let users = fs.readFileSync('./db/users', 'utf8');
         users = JSON.parse(users);
-        var foundUsers;
-        for (var j=0;j<users.length;j++) {
+        let foundUsers;
+        for (let j = 0; j < users.length; j++) {
             if (users[j].email === email) {
                 foundUsers = users[j];
                 break;
             }
         }
         if (foundUsers) {
-            string = string.replace('__password__',foundUsers.password);
+            string = string.replace('__password__', foundUsers.password);
         } else {
-            string = string.replace('__password__','不知道');
+            string = string.replace('__password__', '不知道');
         }
         response.statusCode = 200;
         response.setHeader('Content-Type', 'text/html;charset=utf-8');
         response.write(string);
         response.end();
     } else if (path === '/sign_up' && method === 'GET') {
-        var string = fs.readFileSync('./sign_up.html', 'utf8');
+        let string = fs.readFileSync('./sign_up.html', 'utf8');
         response.statusCode = 200;
         response.setHeader('Content-Type', 'text/html;charset=utf-8');
         response.write(string);
         response.end();
     } else if (path === '/sign_up' && method === 'POST') {
         readBody(request).then(function (body) {
-            var hash = [];
-            var strings = body.split('&'); // ['email=1', 'password=2', 'password_confirmation=2']
+            let hash = [];
+            let strings = body.split('&'); // ['email=1', 'password=2', 'password_confirmation=2']
             strings.forEach(function (string) {
-                var parts = string.split('=');
-                var key = parts[0];
-                var value = parts[1];
+                let parts = string.split('=');
+                let key = parts[0];
+                let value = parts[1];
                 hash[key] = decodeURIComponent(value);
             });
             let {
@@ -83,14 +92,14 @@ var server = http.createServer(function (request, response) {
                 response.statusCode = 400;
                 response.write('password not match');
             } else {
-                var users = fs.readFileSync('./db/users', 'utf8');
+                let users = fs.readFileSync('./db/users', 'utf8');
                 try {
                     users = JSON.parse(users);
                 } catch (exception) {
                     users = [];
                 }
-                var inUse = false;
-                for (var i = 0; i < users.length; i++) {
+                let inUse = false;
+                for (let i = 0; i < users.length; i++) {
                     if (users[i].email === email) {
                         inUse = true;
                         break;
@@ -104,7 +113,7 @@ var server = http.createServer(function (request, response) {
                         email: email,
                         password: password
                     });
-                    var usersString = JSON.stringify(users);
+                    let usersString = JSON.stringify(users);
                     fs.writeFileSync('./db/users', usersString);
                     response.statusCode = 200;
                 }
@@ -112,40 +121,42 @@ var server = http.createServer(function (request, response) {
             response.end();
         });
     } else if (path === '/sign_in' && method === 'GET') {
-        var string = fs.readFileSync('./sign_in.html', 'utf8');
+        let string = fs.readFileSync('./sign_in.html', 'utf8');
         response.statusCode = 200;
         response.setHeader('Content-Type', 'text/html;charset=utf-8');
         response.write(string);
         response.end();
-    } else if (path === '/sign_in' && method === 'POST') {
+    } else if (path === '/sign_in' && method === 'POST') { //登录
         readBody(request).then(function (body) {
-            var hash = [];
-            var strings = body.split('&'); // ['email=1', 'password=2', 'password_confirmation=2']
+            let hash = [];
+            let strings = body.split('&'); // ['email=1', 'password=2', 'password_confirmation=2']
             strings.forEach(function (string) {
-                var parts = string.split('=');
-                var key = parts[0];
-                var value = parts[1];
+                let parts = string.split('=');
+                let key = parts[0];
+                let value = parts[1];
                 hash[key] = decodeURIComponent(value);
             });
             let {
                 email,
                 password
             } = hash;
-            var users = fs.readFileSync('./db/users', 'utf8');
+            let users = fs.readFileSync('./db/users', 'utf8');
             try {
                 users = JSON.parse(users);
             } catch (exception) {
                 users = [];
             }
-            var found = false;
-            for (var i = 0; i < users.length; i++) {
+            let found = false;
+            for (let i = 0; i < users.length; i++) {
                 if (users[i].email === email && users[i].password === password) {
                     found = true;
                     break;
                 }
             }
             if (found) {
-                response.setHeader('Set-Cookie', `sign_in_email=${email}`);
+                let sessionId = parseInt(Math.random() * 1000000); //随机数sessionId
+                sessions[sessionId] = {sign_in_email: email};
+                response.setHeader('Set-Cookie', `sessionId=${sessionId}`);
                 response.statusCode = 200;
             } else {
                 response.statusCode = 401;
@@ -166,7 +177,7 @@ var server = http.createServer(function (request, response) {
 // 封装获取body数据的函数
 function readBody(request) {
     return new Promise(function (resolve, reject) {
-        var body = [];
+        let body = [];
         request.on('data', function (chunk) {
             body.push(chunk);
         }).on('end', function () {
@@ -177,4 +188,4 @@ function readBody(request) {
 }
 
 server.listen(port);
-console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port);
+console.log('监听端口' + port + ' 成功\n请用浏览器打开 http://localhost:' + port);
